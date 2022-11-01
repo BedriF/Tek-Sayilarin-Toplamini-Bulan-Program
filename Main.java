62d0b68694abaf744ae32984d25f78074f6a45fd
Bedri FIDNIK <116302631+BedriF@users.noreply.github.com>
2022-11-01 23:13:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int sayi,toplam=0;
+       do {
+           Scanner input = new Scanner(System.in);
+           System.out.println("Sayı giriniz : ");
+           sayi = input.nextInt();
+           if (sayi %4==0) {
+               toplam += sayi;
+           }
+       }
+           while (sayi %2 == 0) ;
+           System.out.println("Toplam " + toplam);
+    }
+}
